c307e00d616aaa9425a76bb71b0b2a679497e378
Farlan Fadillah <Farlanf61@gmail.com>
2025-08-11 18:53:20 +0700

    initial commit

---
+++ VUE_BASIC/src/components/Home.vue
@@ -0,0 +1,59 @@
+<script setup>
+const hallo         = "<h1>Hello Vue</h1>";
+const classHallo    = "hallo";
+const id            = "hallo";
+
+const buttonDis     = false;
+
+const data = {
+    text : "Youtube",
+    id: "blue",
+    class : "red",
+    href    : "https://www.youtube.com"
+}
+
+const notif = function() {
+    console.info("Hello");
+}
+
+</script>
+
+
+<template>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde molestiae nisi libero, temporibus amet aliquam dignissimos quos animi, voluptatem omnis tenetur aspernatur odio, aperiam et voluptatibus possimus. Ea, eius in.</p>
+
+    <h1 :class ="classHallo" :id >{{ hallo.toUpperCase() }}</h1>
+    <div :class="classHallo" v-html="hallo"></div>
+
+    <button v-on:click="notif" :disabled="buttonDis" >Click This</button>
+    <br>
+    <a  v-bind="data">{{ data["text"] }}</a>
+
+</template>
+
+<style>
+p{
+    color : blue
+}
+
+#hallo {
+    color : green
+}
+
+.hallo {
+    color : red
+}
+
+.holla {
+    color : blue
+}
+
+#blue{
+    color : black
+}
+
+.red {
+    color : orange
+}
+</style>
---
+++ VUE_BASIC/src/components/Test.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { createApp, transformVNodeArgs } from 'vue';
+import {ref} from 'vue';
+
+defineProps({
+    msg : String,
+})
+
+const showButton = ref(false)
+
+const app = createApp(
+    {
+        data() {
+            return {
+                count : 0
+            }
+        }
+    }
+).mount("#buttons")
+
+</script>
+
+<template>
+<h1>{{ msg }}</h1>
+<div id="buttons">
+    <button @click="showButton = !showButton">Show Button</button>
+    <br>
+    <button v-if="showButton">Click Me</button>
+</div>
+
+</template>
+
+<style>
+h1{
+    color : red
+}
+</style>
